a6e1bcb3f50805263a13e8f221ccdbb5708f7ec1
Felipe Mammoli <fmammoli@gmail.com>
2025-09-25 18:48:32 -0300

    adding color picker for stroke

--- app/drawing-canvas.tsx
+++ app/drawing-canvas.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import { Card, CardContent } from "@/components/ui/card";
-import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
+import Color, { ColorLike } from "color";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 import {
   Select,
   SelectContent,
@@ -9,6 +14,15 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  ColorPicker,
+  ColorPickerAlpha,
+  ColorPickerEyeDropper,
+  ColorPickerFormat,
+  ColorPickerHue,
+  ColorPickerOutput,
+  ColorPickerSelection,
+} from "@/components/ui/shadcn-io/color-picker";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { useRef, useState } from "react";
 import { ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas";
@@ -18,7 +32,7 @@ export default function DrawingCanvas() {
   const [eraseMode, setEraseMode] = useState(false);
   const [strokeWidth, setStrokeWidth] = useState(1);
   const [eraserWidth, setEraserWidth] = useState(1);
-
+  const [strokeColor, setStrokeColor] = useState("#000000");
   function handleStrokeClick() {
     setEraseMode(false);
     canvasRef.current?.eraseMode(false);
@@ -36,17 +50,23 @@ export default function DrawingCanvas() {
   function handleEraserWidthChange(value: string) {
     setEraserWidth(Number(value) * 4);
   }
+
+  function handleColorChange(value: ColorLike) {
+    const color = new Color(value);
+    setStrokeColor(color.hex());
+  }
   return (
     <>
       <ReactSketchCanvas
         ref={canvasRef}
         strokeWidth={strokeWidth}
         eraserWidth={eraserWidth}
+        strokeColor={strokeColor}
       ></ReactSketchCanvas>
       <div className="absolute bottom-0 p-8">
         <Menubar className="h-16">
           <MenubarMenu>
-            <ToggleGroup type="single" size={"lg"}>
+            <ToggleGroup type="single" size={"lg"} defaultValue="0">
               <ToggleGroupItem value={"0"} onClick={handleStrokeClick}>
                 Caneta
               </ToggleGroupItem>
@@ -81,6 +101,26 @@ export default function DrawingCanvas() {
                 </SelectContent>
               </Select>
             )}
+            <MenubarTrigger>Color</MenubarTrigger>
+            <MenubarContent className="h-[400px]">
+              <ColorPicker
+                className="max-w-sm rounded-md border bg-background p-4 shadow-sm"
+                onChange={(e) => handleColorChange(e)}
+              >
+                <ColorPickerSelection />
+                <div className="flex items-center gap-4">
+                  <ColorPickerEyeDropper />
+                  <div className="grid w-full gap-1">
+                    <ColorPickerHue />
+                    <ColorPickerAlpha />
+                  </div>
+                </div>
+                <div className="flex items-center gap-2">
+                  <ColorPickerOutput />
+                  <ColorPickerFormat />
+                </div>
+              </ColorPicker>
+            </MenubarContent>
           </MenubarMenu>
         </Menubar>
       </div>
